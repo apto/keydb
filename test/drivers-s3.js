57d50bfc1f2babf281693a22418a8761c7a395ce
darrencruse <darren.cruse@gmail.com>
2014-02-05 14:53:38 -0600

    converted tests from s3-examples.js.tmp into the proper drivers-s3.js mocha test

--- test/drivers-s3.js
+++ test/drivers-s3.js
@@ -9,62 +9,55 @@ describe('s3 driver test', function () {
 
   var db = keydb();
   db.driver(keydb.drivers.media);
+  db.driver(keydb.drivers.s3);
 
-  // just for initial testing the below temporarily includes hard coded credentials: 
-  // (these need to be removed at some point)
-  db.driver(keydb.drivers.s3, 
-    { 
-      "accessKeyId": "AKIAJCHBC5JUFSW7P7KA", 
-      "secretAccessKey": "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", 
-      "region": "us-east-1" 
-    }
-  );
-
-/*
   it('should set value on root', function () {
     var promise = db({op: 'set', key: 'foo', value: 'bar'});
     return expect(promise).to.be.fulfilled;
   });
-*/  
+
   it('should get value on root', function () {
     var promise = db({op: 'get-string', key: 'foo'});
-    promise.then(function (result) {
-      console.log(typeof result);
-      console.log(result);
-    }).fail(function (err) {
-      console.log('Failed: ' + err);
-    })
-    return expect(promise).to.eventually.eql('bar');
+    return expect(promise).to.eventually.eql({ key: 'foo', value: '"bar"' });
   });
-/*
+
+  it('should fail to get nonexistent file \'abc\'', function () {
+    var promise = db({op: 'get', key: 'abc'})
+    expect(promise).to.be.rejectedWith(keydb.error.NotFound);    
+  });
+
+  it('should get meta for file', function () {
+    var promise = db({op: 'meta', key: 'foo'})
+    return expect(promise).to.eventually.eql({ key: 'foo' });
+  });
+
+  it('should fail to get meta for nonexistent file \'abc\'', function () {
+    var promise = db({op: 'meta', key: 'abc'})
+    expect(promise).to.be.rejectedWith(keydb.error.NotFound);    
+  });
+
   it('should delete value on root', function () {
     return db({op: 'delete', key: 'foo', value: 'bar'});
   });
-*/
-/*
+
   it('should fail to get deleted value on root', function () {
     var promise = db({op: 'get-string', key: 'foo'}).then(function (msg) {
       return msg.value;
     });
     expect(promise).to.be.rejectedWith(keydb.error.NotFound);
   });
-*/
-/*
-  it('should set collection on root', function () {
-    var promise = db({op: 'set', type: 'collection', key: 'fooFolder'});
-    return expect(promise).to.be.fulfilled;
-  });
-*/
-  /*
+
   it('should create a collection', function () {
     return db({op: 'set', key: 'users', type: 'collection', value: {}});
   });
+
   it('should set a value on a collection', function () {
     return db({op: 'set', key: 'users/joe', value: {fn: 'Joe'}});
   });
+
   it('should get a value on a collection', function () {
-    var promise = db({op: 'get', key: 'users/joe'}).then(function (msg) {
-      return msg.value;
+    var promise = db({op: 'get-string', key: 'users/joe'}).then(function (msg) {
+      return JSON.parse(msg.value);
     });
     return expect(promise).to.eventually.eql({fn: 'Joe'});
   });
@@ -76,5 +69,17 @@ describe('s3 driver test', function () {
       ]);
     });
   });
-*/
+
+  it('should get meta for a collection', function () {
+    var promise = db({op: 'meta', key: 'users'});
+    return expect(promise).to.eventually.eql({ key: 'users', type: 'collection' });
+  });
+
+  it('should create a file from a stream', function () {
+    var fs = require('fs');
+    var fileStream = fs.createReadStream('./drivers-s3.js');
+    var promise = db({op: 'set', key: 'drivers-s3.js', value: fileStream});
+    return expect(promise).to.be.fulfilled;
+  });
+
 });
