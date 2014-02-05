134439875b4d598e8a463ba1f22e924ae26ceb93
subhrajit <s.samal@elsevier.com>
2014-02-05 16:39:40 -0600

    added the review comments and added the set collection and delete and unit test codes

--- test/drivers-ftp.js
+++ test/drivers-ftp.js
@@ -2,42 +2,56 @@ var chai = require('chai');
 var expect = chai.expect;
 var keydb = require('keydb');
 var Stream = require('stream');
+var fs = require('fs');
 
 require('mocha-as-promised')();
 chai.use(require('chai-as-promised'));
 describe('ftp driver test', function () {
   var db = keydb();
   db.driver(keydb.drivers.media);
-  db.driver(keydb.drivers.ftp);
-
-  it('should set value on /usr/local/foo.txt', function () {
-    var stream = new Stream();
-
-    stream.pipe = function (dest) {
-      dest.write('bar');
-    };
-    var promise = db({op: 'set', key: '/usr/local/foo.txt', value: stream});
+  db.driver(keydb.drivers.ftp, {
+    username : 'samals',
+    password : 'bapuna@44'
+  });
+  before(function () {
+    fs.writeFile("/tmp/test.txt", "bar");
+    fs.writeFile("/tmp/delete.txt", "this file will be deleted");
+  });
+  it('should set value on foo.txt', function () {
+    var st = fs.createReadStream('/tmp/test.txt');
+    var promise = db({op: 'set', key: '/tmp/foo.txt', value: st});
     return expect(promise).to.be.fulfilled;
   });
-  it('should get value on foo.txt', function () {
-    var promise = db({op: 'get-string', key: '/usr/local/foo.txt'});
-    promise.then(function (result) {
-      console.log(typeof result);
-      console.log(result);
-    }).fail(function (err) {
-      console.log('Failed: ' + err);
+  it('should set a directory ', function () {
+    var promise = db({op: 'set', key: '/tmp/test', type : "collection"});
+    return expect(promise).to.be.fulfilled;
+  });
+  it('should delete ', function () {
+    var promise = db({op: 'delete', key: '/tmp/delete.txt', type : "collection"});
+    return expect(promise).to.be.fulfilled;
+  });
+  it('should get value on test.txt', function () {
+    var promise = db({op: 'get-string', key: '/tmp/test.txt'}).then(function (result) {
+      return result.value;
+
     });
-    return expect(promise).to.eventually.eql('bar');
+    return expect(promise).to.eventually.eql("bar");
+  });
+  it('should get a directory', function () {
+    var promise = db({op: 'get', key: '/tmp'}).then(function (result) {
+      expect(result.value.length).to.be.above(0);
+    });
   });
   it('should get the meta on foo.txt', function () {
-    var promise = db({op: 'meta', key: '/usr/local/foo.txt'});
-    promise.then(function (result) {
-      console.log(typeof result);
-      console.log(result);
-      return expect(result.type).to.eql('file');
-    }).fail(function (err) {
-      console.log('Failed: ' + err);
+    var promise = db({op: 'meta', key: '/tmp/test.txt'}).then(function (result) {
+      return result.type;
     });
+    return expect(promise).to.eventually.eql('file');
     
+  });
+
+  after(function () {
+    fs.unlinkSync("/tmp/test.txt");
+    fs.rmdirSync("/tmp/test");
   });
 });
